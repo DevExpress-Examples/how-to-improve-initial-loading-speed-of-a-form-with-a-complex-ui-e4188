e5a1404b8440c20c687e99ec3059a1e968c54d8d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/EditForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RichEditLoadingSpeed {
+    public partial class EditForm : Form {
+        public EditForm() {
+            InitializeComponent();
+        }
+    }
+}
